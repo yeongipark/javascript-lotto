3619268c1d2a17d011f26343baf521ef8f1532f1
yeongipark <yeongi0111@naver.com>
2025-02-23 18:34:36 +0900

    refactor(Calculator) : reduce를 이용해 로직 단순화
    - getStatistics 메서드의 내부 로직에 reduce를 상용해 로직 단순화

--- src/domain/Calculator.js
+++ src/domain/Calculator.js
@@ -2,19 +2,37 @@ import { LOTTO_PRISE, RANK } from "../constant/definition.js";
 export default class Calculator {
   // [{matchCount : 1, isMatchBonusNumber : true}]
   static getStatistics(matchCounts) {
-    const rank = {
-      [RANK.FIFTH]: 0,
-      [RANK.FOURTH]: 0,
-      [RANK.THIRD]: 0,
-      [RANK.SECOND]: 0,
-      [RANK.FIRST]: 0,
-    };
-
-    matchCounts.forEach((matchData) => {
-      this.#judgeRank(matchData, rank);
-    });
-
-    return rank;
+    return matchCounts.reduce(
+      (acc, { matchCount, isMatchBonusNumber }) => {
+        switch (matchCount) {
+          case 3: {
+            acc[RANK.FIFTH]++;
+            break;
+          }
+          case 4: {
+            acc[RANK.FOURTH]++;
+            break;
+          }
+          case 5: {
+            if (isMatchBonusNumber) acc[RANK.SECOND]++;
+            else acc[RANK.THIRD]++;
+            break;
+          }
+          case 6: {
+            acc[RANK.FIRST]++;
+            break;
+          }
+        }
+        return acc;
+      },
+      {
+        [RANK.FIFTH]: 0,
+        [RANK.FOURTH]: 0,
+        [RANK.THIRD]: 0,
+        [RANK.SECOND]: 0,
+        [RANK.FIRST]: 0,
+      }
+    );
   }
 
   static #judgeRank({ matchCount, isMatchBonusNumber }, rank) {
