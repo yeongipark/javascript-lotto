76db2ddd57074577d27956e416fe65f20d3d59a1
yeongipark <yeongi0111@naver.com>
2025-02-27 18:59:08 +0900

    refactor(dom): 접근해야 하는 노드 변수 저장

---
+++ src/view/dom.js
@@ -0,0 +1,22 @@
+const domRefs = {
+  $buyButton: document.querySelector(".paper_buy_button"),
+  $inputPrice: document.querySelector(".paper_input_price"),
+  $$lottoInfoWrap: document.querySelector(".paper_lotto_information_wrap"),
+  $lottoInfo: document.querySelector(".paper_lotto_information"),
+  $lottoCount: document.querySelector(".paper_lotto_count"),
+  $resultButton: document.querySelector(".paper_result_button"),
+  modal: document.querySelector(".result_modal"),
+  $paper_winning_number_inputs: document.querySelectorAll(
+    ".paper_winning_number_input"
+  ),
+  $paper_bonus_number_input: document.querySelector(
+    ".paper_bonus_number_input"
+  ),
+  $modalCloseButton: document.querySelector(".modal_close_button"),
+  $modalRestartButton: document.querySelector(".modal_restart_button"),
+  $matchCounts: document.querySelectorAll(".modal_match_count"),
+  $winningRate: document.querySelector(".modal_winning_rate"),
+  $lottoMachine: null, // 초기값 설정
+};
+
+export default domRefs;
