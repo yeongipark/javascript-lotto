83c819ea2085cf57e02ae6106a2179e1fe2be593
yeongipark <yeongi0111@naver.com>
2025-02-28 17:05:06 +0900

    feat(modalHandler): modal 배경 클릭하면 닫힘 기능 추가

--- src/webView/modalHandler.js
+++ src/webView/modalHandler.js
@@ -3,3 +3,9 @@ import domRefs from "./dom.js";
 domRefs.$modalCloseButton.addEventListener("click", () => {
   domRefs.$modal.close();
 });
+
+domRefs.$modal.addEventListener("click", (e) => {
+  if (e.target === domRefs.$modal) {
+    domRefs.$modal.close();
+  }
+});
